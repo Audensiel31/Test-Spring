9756cd238088bd2f900d26d39a8a85616dd2e07a
Audensiel31 <58598706+Audensiel31@users.noreply.github.com>
2019-12-10 12:09:48 +0100

    Update IEleveService.java
    add comment on service

--- src/main/java/com/audensiel/eleve/service/IEleveService.java
+++ src/main/java/com/audensiel/eleve/service/IEleveService.java
@@ -6,8 +6,21 @@ import com.audensiel.eleve.domain.Eleve;
 
 public interface IEleveService {
 
+	/**
+	 * Liste de tout les élèves en bdd
+	 */
 	public List<Eleve> getListeEleve();
+	/**
+	 * Meilleur élève d'une matière
+	 */
 	public Eleve getMeilleurEleveParMatiere(String matiere);
+	/**
+	 * Meilleur élève toute matière cofondue
+	 */
 	public Eleve getMeilleurEleve();
+	/**
+	 * Synthèse des élèves de la classe sous la forme: 
+	 * Eleve : {Nom} {Prenom} {Moyenne} {Classement}
+	 */
 	public String getSyntheseEleveClasse();
 }
